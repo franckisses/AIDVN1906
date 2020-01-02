c61eefbc10792d40d9e3e35d19b6679a38d369f6
franckisses <franck_gxu@outlook.com>
2020-01-02 20:44:28 +0800

    register view

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
+import json
+import hashlib
+
+from .models import UserProfile
 
 # Create your views here.
 
@@ -12,7 +16,58 @@ class UserRegisterView(View):
         pass
 
     def post(self,request):
-        print('test')
-        return JsonResponse({
-            'code':200
-        })
+        """
+        1. 获取浏览器前段传递的数据
+        2. 验证前端传递的数据。
+        3. 判断用户是不是在我们数据库中存在。
+        4. 判断用户密码是否合法。对密码进行处理
+        5. 创建用户。生成ｔｏｋｅｎ.返回给前端。
+        """
+        json_obj = request.body
+        if not json_obj:
+            return JsonResponse({'code':201,'error':'no content!'})
+        json_dict = json.loads(json_obj)
+
+        # 验证用户名
+        username = json_dict.get('username',None)
+        if not username:
+            return JsonResponse({'code':202,'error':'no username'})
+        # 获取邮箱。获取密码
+        email = json_dict.get('email',None)
+        if not email:
+            return JsonResponse({'code':203,'error':'no email'})
+        password_1 = json_dict.get('password_1',None)
+        password_2 = json_dict.get('password_2',None)
+        if not password_1 or not password_2:
+            return JsonResponse({'code':204,'error':'no password'})
+        # 密码１　和　密码２　需要一致
+        if password_1 != password_2:
+            return JsonResponse({'code':205,'error':'different password!'})
+        old_user = UserProfile.objects.get(username=username)
+        if old_user:
+            return JsonResponse({'code':206,'error':'user already existed!'})
+        m = hashlib.md5()
+        m.update(password_1.encode())
+        sign = info = " "
+        try:
+            UserProfile.objects.create(
+                username=username,
+                nickname=username,
+                password=m.hexdigest(),
+                sign=sign,
+                info=info, 
+                email=email
+            )
+        except Exception as e：
+            return JsonResponse({'code':207,'error':'server is busy!'})
+            #  21:00 回来
+
+
+
+
+            
+
+        
+        print(json_obj ,type(json_obj))
+
+        return JsonResponse({'code':200})
