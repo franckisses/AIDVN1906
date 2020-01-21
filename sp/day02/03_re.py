7f0d8761bf3c4f82afff7511acd67630bd1e7907
franckisses <franck_gxu@outlook.com>
2020-01-21 21:13:46 +0800

    test

--- sp/day02/03_re.py
+++ sp/day02/03_re.py
@@ -4,7 +4,6 @@ s = 'A B C D'
 p1 = re.compile('\w+\s+\w+')
 print(p1.findall(s))
 # 结果: ['A B','C D']
-
 p2 = re.compile('(\w+)\s+\w+')
 print(p2.findall(s))
 # 第1步:匹配完整 -> ['A B','C D']
@@ -14,4 +13,4 @@ p3 = re.compile('(\w+)\s+(\w+)')
 print(p3.findall(s))
 # 第1步:匹配完整 -> ['A B','C D']
 # 第二步 [('A','B'),('C','D')]
-# 9:08
+# 9:08 
---
+++ sp/day02/04_re_group.py
@@ -0,0 +1,25 @@
+html = """
+<div class="animal">
+    <p class="name">
+		<a title="Tiger"></a>
+    </p>
+    <p class="content">
+		Two tigers two tigers run fast
+    </p>
+</div>
+<div class="animal">
+    <p class="name">
+		<a title="Rabbit"></a>
+    </p>
+    <p class="content">
+		Small white rabbit white and white
+    </p>
+</div>"""
+import re
+res = '<div class="animal">.*?title="(.*?)".*?"content">(.*?)</p>'
+pattern = re.compile(res,re.S)
+re_list = pattern.findall(html)
+print(re_list)
+"""
+[('Tiger', '\n\t\tTwo tigers two tigers run fast\n    '), ('Rabbit', '\n\t\tSmall white rabbit white and white\n    ')]
+"""
