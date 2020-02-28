ae445fa797d519e54cac231f7d47646d363dde94
franck <g-gongyan@tedu.cn>
2020-02-28 20:06:28 +0800

    find images element

--- sp/day15/07_jdSpider.py
+++ sp/day15/07_jdSpider.py
@@ -24,20 +24,45 @@ class JdSpider:
         time.sleep(1)
         self.browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')
         time.sleep(5)
+        # 当前元素的节点是60个
         all_products = self.browser.find_elements_by_xpath('//li[@class="gl-item"]/div/div[1]/a/img')
         print('找到了%d个'%(len(all_products)))
-        for i in all_products:
-            # WebElement.get_attribute('class') # pn-next / pn-next disable
-            print(i.get_attribute('src'))
-            # time.sleep(10)
-            # print(self.browser.window_handles)
+        # 通过二级页面进行解析所有的商品数据
+        self.parse_two_html(all_products)
+    
+
+
+    def parse_two_html(self,products):
+        """
+            此方法是用来解析二级页面
+        """
+        for each_product in products:
+            each_product.click()
+            self.browser.switch_to_window(self.browser.window_handles[1])
+            # 手机的标题
+            title = self.browser.find_element_by_xpath(
+                '/html/body/div[8]/div/div[2]/div[1]'
+                ).text.strip()
+            # 手机的颜色
+            colors = self.browser.find_element_by_xpath(
+                '//*[@id="choose-attr-1"]/div[2]'
+            ).text
+            # 手机的规格
+            types = self.browser.find_element_by_xpath(
+                '//*[@id="choose-attr-2"]/div[2]'
+            ).text
+            # 图片的链接
+            images_element = self.browser.find_elements_by_xpath(
+                '//*[@id="spec-list"]/ul/li/img'
+                ) # WebElement
+            images = [ i.get_attribute('src') for i in images_element]
+            print('title',title)
+            print('color',colors)
+            print('type',types)
+            print('img',images)
             break
-        # print(all_products)
-        # print(len(all_products))
 
-
-    def parse_two_html(self):
-        pass
+        time.sleep(10)
 
     def main(self):
         self.parse_one_html()
@@ -46,3 +71,9 @@ class JdSpider:
 if __name__ == "__main__":
     jd = JdSpider()
     jd.main()
+
+# 今日内容：
+    # 1.京东爬虫
+    # 2.b站滑动链登陆
+    # # 7:30 开始
+# https://img12.360buyimg.com/n5/s54x54_jfs/t1/102595/3/12610/144213/5e4cb782Ee6ce6e44/894d26bdcaaacbd3.jpg
