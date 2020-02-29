f0c94593e86a518385be75c1135997b994f0b2d8
franck <g-gongyan@tedu.cn>
2020-02-29 20:52:20 +0800

    test

--- sp/day17/bilibili.py
+++ sp/day17/bilibili.py
@@ -35,10 +35,35 @@ class BiliibiliSpider:
         # 填充账户名和密码
         username_element.send_keys(self.username)
         password_element.send_keys(self.passwd)
+        # 找到点击登陆按钮
+        button_login = self.browser.find_element_by_xpath(
+            '//*[@id="geetest-wrap"]/div/div[5]/a[1]'
+        )
+        try:
+            button_login.click()
+        except Exception as e:
+            print('[Error]:',e)
 
+    def save_bg(self,filename,canvas_class):
+        """
+            保存图片
+            1.图片名字，
+            2.节点的名字
+        """
+        # full_image = ''
+        time.sleep(3)
+        getImages = "return document.getElementsByClassName('"+canvas_class+"')[0].toDataURL('image/png');"
+        print(getImages)
+        images_data = self.browser.execute_script(getImages)
+        
+        print(images_data[:20])
 
     def main(self):
+        # 登陆触发检测
         self.login()
+        # 获取完整图片 以及有缺口的图片
+        self.save_bg('bg.png','geetest_canvas_bg geetest_absolute')
+
 
 if __name__ == "__main__":
     import getpass
